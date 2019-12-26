ec9a5770efce9c26bd3cfb885a11df5adaed268b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-12-25 20:02:03 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kristen Doucette</h1>
+      <h2> Full-Stack Web Developer</h2>
+      <p> I am A video game enthusiast. Environmentalist.</p>
     </div>
   );
 }
